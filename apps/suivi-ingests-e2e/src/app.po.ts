41af19e721c5b9bc37f585bdabe5b2a13c1b9e5f
Daphne <remalidaphne@gmail.com>
2018-06-14 14:05:00 +0200

    add suivi-ingests app

---
+++ apps/suivi-ingests-e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('ab-ng-nx-project-root h1')).getText();
+  }
+}
---
+++ apps/suivi-ingests/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ab-ng-nx-project-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ab-ng-nx-project';
+}
